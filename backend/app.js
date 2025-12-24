df7f730b277f170994f3e7640ed4fe4e9be4dab5
Cameron Stanley <cam96stanley@gmail.com>
2025-12-24 11:37:05 -0500

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const invoiceRoutes = require('./routes/invoice');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
